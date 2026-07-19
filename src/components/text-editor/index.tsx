b542033f3db02b09b86cb889c394899e7fd69ca3
zeth porter <zethwporter@gmail.com>
2026-07-18 20:50:14 -0600

    added tip tap example

---
+++ src/components/text-editor/index.tsx
@@ -0,0 +1,25 @@
+// src/Tiptap.tsx
+import { useEditor, EditorContent, EditorContext } from '@tiptap/react'
+import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus'
+import StarterKit from '@tiptap/starter-kit'
+import { useMemo } from 'react'
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [StarterKit], // define your extension array
+    content: '<p>Hello World!</p>', // initial content
+  })
+
+  // Memoize the provider value to avoid unnecessary re-renders
+  const providerValue = useMemo(() => ({ editor }), [editor])
+
+  return (
+    <EditorContext.Provider value={providerValue}>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </EditorContext.Provider>
+  )
+}
+
+export default Tiptap
