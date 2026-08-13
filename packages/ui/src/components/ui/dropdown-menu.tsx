886594901ac1959efe8c1bbab490430c68d56700
zeth <zethwporter@gmail.com>
2026-08-13 00:51:35 -0600

    installed package updated formatting

--- packages/ui/src/components/ui/dropdown-menu.tsx
+++ packages/ui/src/components/ui/dropdown-menu.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
 import { Menu as MenuPrimitive } from '@base-ui/react/menu';
-
+import { CheckIcon, ChevronRightIcon } from 'lucide-react';
+import * as React from 'react';
 import { cn } from '../../lib/utils.ts';
-import { ChevronRightIcon, CheckIcon } from 'lucide-react';
 
 function DropdownMenu({ ...props }: MenuPrimitive.Root.Props) {
 	return <MenuPrimitive.Root data-slot='dropdown-menu' {...props} />;
@@ -253,18 +252,18 @@ function DropdownMenuShortcut({
 
 export {
 	DropdownMenu,
-	DropdownMenuPortal,
-	DropdownMenuTrigger,
+	DropdownMenuCheckboxItem,
 	DropdownMenuContent,
 	DropdownMenuGroup,
-	DropdownMenuLabel,
 	DropdownMenuItem,
-	DropdownMenuCheckboxItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
 	DropdownMenuRadioGroup,
 	DropdownMenuRadioItem,
 	DropdownMenuSeparator,
 	DropdownMenuShortcut,
 	DropdownMenuSub,
-	DropdownMenuSubTrigger,
 	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
 };
