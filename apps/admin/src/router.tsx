917a48f72b9b6b646dfc38ff7991bb4c043b5777
zeth <zethwporter@gmail.com>
2026-08-13 00:57:38 -0600

    testing signatures

--- apps/admin/src/router.tsx
+++ apps/admin/src/router.tsx
@@ -1,7 +1,6 @@
 import { QueryClient } from '@tanstack/react-query';
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
-import type { ReactNode } from 'react';
 import TanstackQueryProvider, {
 	getContext,
 } from './integrations/tanstack-query/root-provider';
