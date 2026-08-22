62a6af9b456dc4054f46eb600a5deca98231fb71
zeth <zethwporter@gmail.com>
2026-08-21 23:41:34 -0600

    auth db migration

---
+++ packages/auth/authMigrations/20260822053103_smooth_jubilee/migration.sql
@@ -0,0 +1,43 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_session` (
+	`id` text PRIMARY KEY,
+	`expires_at` integer NOT NULL,
+	`token` text NOT NULL UNIQUE,
+	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	`updated_at` integer NOT NULL,
+	`ip_address` text,
+	`user_agent` text,
+	`user_id` text NOT NULL,
+	`active_organization_id` text,
+	`impersonated_by` text,
+	CONSTRAINT `session_user_id_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+);
+--> statement-breakpoint
+INSERT INTO `__new_session`(`id`, `expires_at`, `token`, `created_at`, `updated_at`, `ip_address`, `user_agent`, `user_id`, `active_organization_id`, `impersonated_by`) SELECT `id`, `expires_at`, `token`, `created_at`, `updated_at`, `ip_address`, `user_agent`, `user_id`, `active_organization_id`, `impersonated_by` FROM `session`;--> statement-breakpoint
+DROP TABLE `session`;--> statement-breakpoint
+ALTER TABLE `__new_session` RENAME TO `session`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_user` (
+	`id` text PRIMARY KEY,
+	`name` text NOT NULL,
+	`email` text NOT NULL UNIQUE,
+	`email_verified` integer DEFAULT false NOT NULL,
+	`image` text,
+	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
+	`two_factor_enabled` integer DEFAULT false,
+	`role` text,
+	`banned` integer DEFAULT false,
+	`ban_reason` text,
+	`ban_expires` integer
+);
+--> statement-breakpoint
+INSERT INTO `__new_user`(`id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at`, `two_factor_enabled`, `role`, `banned`, `ban_reason`, `ban_expires`) SELECT `id`, `name`, `email`, `email_verified`, `image`, `created_at`, `updated_at`, `two_factor_enabled`, `role`, `banned`, `ban_reason`, `ban_expires` FROM `user`;--> statement-breakpoint
+DROP TABLE `user`;--> statement-breakpoint
+ALTER TABLE `__new_user` RENAME TO `user`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+DROP INDEX IF EXISTS `organization_slug_unique`;--> statement-breakpoint
+DROP INDEX IF EXISTS `session_token_unique`;--> statement-breakpoint
+DROP INDEX IF EXISTS `user_email_unique`;--> statement-breakpoint
+CREATE INDEX `session_userId_idx` ON `session` (`user_id`);
