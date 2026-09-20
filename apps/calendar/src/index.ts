8f3a65aeab2e9773f928d6ca782eb26f4302a0f6
zeth <zethwporter@gmail.com>
2026-09-20 01:20:03 -0600

    adding calendar api

--- apps/calendar/src/index.ts
+++ apps/calendar/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { createCalendar } from './fns/create-calendar';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Will match `/api/animal` and `/api/animal/:type`
+app.get('/:calendarId/:startDate?/:endDate?/calendar.ics', async (c) => {
+	const { calendarId, startDate, endDate } = c.req.param();
+	const calendar = await createCalendar({
+		calendarId,
+		startDate: startDate ? Number(startDate) : undefined,
+		endDate: endDate ? Number(endDate) : undefined,
+	});
+	return c.body(calendar.toString(), 200, {
+		'Content-Type': 'text/calendar; charset=utf-8',
+		'Content-Disposition': 'attachment; filename="calendar.ics"',
+	});
+});
 
-export default app
+export default app;
+
+// import { createCalendar } from './fns/create-calendar';
+
+// const main = async () => {
+// 	const icalendar = await createCalendar({
+// 		calendarId: 'CAeNFKyyKBcu-2k_t4BvN',
+// 	});
+
+// 	console.log(icalendar);
+// };
+// main();
